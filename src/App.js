5611b710fe763f29c759ab69c1926d945af82888
Adam Marc Williams <adam@adammarcwilliams.co.uk>
2016-10-11 11:15:37 +0100

    Enhanced gameplay UX and practiced graceful degradation
    - Throttled calculateScore function using _.debounce for suspense
    - Added botWeaponSelect animation
    - Created a reset button with media query for flex order
    - Added graceful degradation to hide GSAP animation on mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import TweenMax, { TimelineMax, Elastic, Linear } from 'gsap'
+import { TweenMax, TimelineMax, Elastic, Linear } from 'gsap'
+import * as _ from 'underscore'
 import Weapons from './Weapons'
 import Scores from './Scores'
 import './App.css'
@@ -14,6 +15,8 @@ class App extends Component {
       result: "Select your weapon..."
     }
     this.handleSelect = this.handleSelect.bind(this)
+    this.handleReset = this.handleReset.bind(this)
+    this.calculateWinner = _.debounce(this.calculateWinner, 1000)
   }
   componentDidMount () {
     const TAGLINE = document.getElementById('tagline')
@@ -26,42 +29,73 @@ class App extends Component {
     const BACKGROUND = document.getElementById('background')
     const RESULT = document.getElementById('result')
     const SCOREBOX = document.getElementById('scoreBox')
-    const INTRO_TL = new TimelineMax()
 
+    if (window.innerWidth > 700) {
 
-
-    INTRO_TL.fromTo(ROCK, 1, {x: -1500, y: -200}, {x: 200, y:-200, ease:Elastic.easeInOut})
-            .from(PAPER, 1, {x: -1500, ease:Elastic.easeInOut}, .1)
-            .fromTo(SCISSORS, 1, {x: -1500, y: 200}, {x: -200, y: 200, ease:Elastic.easeInOut, onComplete: eyesAnimation}, .2)
-            .to(ROBOT_EYES, .5, {autoAlpha: 1}, 1)
-
-    function eyesAnimation() {
-      const EYES_TL = new TimelineMax({repeat: 1, yoyo:true})
-      EYES_TL.to(ROBOT_EYE_LEFT, .1, {css: {transform: 'rotate(110deg)'}})
-            .to(ROBOT_EYE_RIGHT, .1, {css: {transform: 'rotate(-110deg)'}}, 0)
-            .to(BACKGROUND, .1, {css: {background: 'linear-gradient(45deg, #7000B2 0%,#E8600C 100%)'}, onComplete: animateOut}, .1)
+      function animateIn() {
+        const INTRO_TL = new TimelineMax()
+        INTRO_TL.fromTo(ROCK, 1, {x: -1500, y: -200}, {x: 200, y:-200, ease:Elastic.easeInOut})
+                .from(PAPER, 1, {x: -1500, ease:Elastic.easeInOut}, .1)
+                .fromTo(SCISSORS, 1, {x: -1500, y: 200}, {x: -200, y: 200, ease:Elastic.easeInOut}, .2)
+                .to(ROBOT_EYES, .5, {autoAlpha: 1, onComplete: eyesAnimation}, 1)
+      }
+      function eyesAnimation() {
+        const EYES_TL = new TimelineMax({repeat: 1, yoyo:true})
+        EYES_TL.to(ROBOT_EYE_LEFT, .1, {css: {transform: 'rotate(110deg)'}})
+                .to(ROBOT_EYE_RIGHT, .1, {css: {transform: 'rotate(-110deg)'}}, 0)
+                .to(BACKGROUND, .1, {css: {background: 'linear-gradient(45deg, #7000B2 0%,#E8600C 100%)'}, onComplete: animateOut}, .1)
+      }
+      function animateOut() {
+        const ANIMATE_OUT = new TimelineMax()
+        ANIMATE_OUT.to(ROBOT_EYES, 0.5, {autoAlpha: 0})
+                    .to(ROCK, 0.5, {x: 0, y:0, ease:Elastic.easeInOut})
+                    .to(SCISSORS, 0.5, {x: 0, y:0, ease:Elastic.easeInOut})
+                    .to(TAGLINE, 0.5, {autoAlpha: 1, ease:Linear.easeIn})
+                    .to([RESULT, SCOREBOX], 1, {autoAlpha: 1, ease:Linear.easeIn})
+      }
+      animateIn()
     }
-    function animateOut() {
-      const ANIMATE_OUT = new TimelineMax()
-      ANIMATE_OUT.to(ROBOT_EYES, 1, {autoAlpha: 0})
-                .to(ROCK, 0.5, {x: 0, y:0, ease:Elastic.easeInOut})
-                .to(SCISSORS, 0.5, {x: 0, y:0, ease:Elastic.easeInOut})
-                .to(TAGLINE, 0.5, {autoAlpha: 1, ease:Linear.easeIn})
-                .to([RESULT, SCOREBOX], 1, {autoAlpha: 1, ease:Linear.easeIn})
-    }
-
-
-
   }
   handleSelect (e) {
     let weaponSelected = e.target.id
     this.botSelect(weaponSelected)
   }
+  handleReset () {
+    this.setState({
+      playerScore: 0,
+      botScore: 0,
+      result: "Select your weapon..."
+    })
+  }
+
   botSelect (playerWeapon) {
     const WEAPONS = ['rock', 'paper', 'scissors']
     let randomNumber = () => Math.floor(Math.random() * 3)
     let botWeapon = WEAPONS[randomNumber()]
     this.calculateWinner(playerWeapon, botWeapon)
+
+    function botSelectElement(botWeapon) {
+      const ROCK = document.getElementById('rockDiv')
+      const PAPER = document.getElementById('paperDiv')
+      const SCISSORS = document.getElementById('scissorsDiv')
+      const WEAPON_BOX = document.getElementById('weaponBox')
+
+      if (botWeapon === 'rock') {
+        botSelectAnimate(ROCK)
+      }
+      if (botWeapon === 'paper') {
+        botSelectAnimate(PAPER)
+      }
+      if (botWeapon === 'scissors') {
+        botSelectAnimate(SCISSORS)
+      }
+      function botSelectAnimate(weapon) {
+        const BOT_SELECT_TL = new TimelineMax()
+        BOT_SELECT_TL.to(WEAPON_BOX, .1, {repeat: 5, yoyo: true, y: 5, ease:Linear.easeInOut})
+                      .to(weapon, .2, {repeat: 1, yoyo: true, scale: 2, ease:Elastic.easeInOut})
+      }
+    }
+    botSelectElement(botWeapon)
   }
   calculateWinner (playerWeapon, botWeapon) {
     if (playerWeapon === botWeapon) {
@@ -94,6 +128,7 @@ class App extends Component {
           <Weapons handleSelect={this.handleSelect}/>
           <p id="result" className="App-result">{this.state.result}</p>
           <Scores
+            handleReset={this.handleReset}
             playerScore={this.state.playerScore}
             botScore={this.state.botScore}/>
         </div>
--- src/Scores.js
+++ src/Scores.js
@@ -4,11 +4,14 @@ const Scores = (props) => {
 
   return (
     <div id="scoreBox" className="App-scores-box">
-      <div className="App-scores-col">
+      <div className="App-scores-col--human">
         <p className="App-scores--label">HUMAN</p>
         <p className="App-scores--counter">{props.playerScore}</p>
       </div>
-      <div className="App-scores-col">
+      <div className="App-scores-col--reset">
+        <a href="#" className="App-scores-reset" onClick={props.handleReset}>RESET GAME</a>
+      </div>
+      <div className="App-scores-col--robot">
         <p className="App-scores--label">ROBOT</p>
         <p className="App-scores--counter">{props.botScore}</p>
       </div>
